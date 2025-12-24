0f60d31fe1f3326ecba9a8887977f381cd58c8a3
Braz Suthar <brazsuthar@gmail.com>
2025-12-24 21:13:21 +0530

    Update WebRTC transport configuration to support announced IP
    - Modified the createWebRtcTransport function to utilize the MEDIASOUP_ANNOUNCED_IP environment variable for the announced IP address, enhancing flexibility in network configurations.

--- mediasoup-server/src/transports.js
+++ mediasoup-server/src/transports.js
@@ -1,6 +1,8 @@
+const ANNOUNCED_IP = process.env.MEDIASOUP_ANNOUNCED_IP || null;
+
 export async function createWebRtcTransport(router) {
     return router.createWebRtcTransport({
-        listenIps: [{ ip: "0.0.0.0", announcedIp: null }],
+        listenIps: [{ ip: "0.0.0.0", announcedIp: ANNOUNCED_IP }],
         enableUdp: true,
         enableTcp: true,
         preferUdp: true
